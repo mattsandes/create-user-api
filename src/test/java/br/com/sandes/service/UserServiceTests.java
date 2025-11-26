fe3a27e0d3b9331fd43a09ecf6af6c37efbd475a
Mateus Sandes <mateussandes@hotmail.com.br>
2025-11-26 10:27:52 -0300

    Adicionado testes para o find all e find by id

--- src/test/java/br/com/sandes/service/UserServiceTests.java
+++ src/test/java/br/com/sandes/service/UserServiceTests.java
@@ -14,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -28,16 +30,24 @@ public class UserServiceTests {
     @InjectMocks
     private UserService userService;
 
-    private User user;
+    private User user1;
+    private User user2;
     private CreateUserDTO userDTO;
 
     @BeforeEach
     void setUp() {
-        user = new User(
+        user1 = new User(
             1L,
-            "Test User",
-            "test.user@gmail.com",
+            "Test User 1",
+            "test.user1@gmail.com",
             LocalDateTime.now()
+        );
+
+        user2 = new User(
+                2L,
+                "Test User 2",
+                "test.user2@gmail.com",
+                LocalDateTime.now()
         );
 
         userDTO = new CreateUserDTO(
@@ -50,7 +60,7 @@ public class UserServiceTests {
     @DisplayName("Validar que é possivel criar um novo usuario")
     void Create_Should_ReturnANewUser_When_AValidUserIsPassed() {
 
-        when(userRepository.create(any(User.class))).thenReturn(user);
+        when(userRepository.create(any(User.class))).thenReturn(user1);
 
         var createUser = userService.create(userDTO);
 
@@ -59,6 +69,33 @@ public class UserServiceTests {
                 .create(any(User.class));
 
         assertInstanceOf(CreatedUserResponse.class, createUser);
+    }
+
+    @Test
+    @DisplayName("Validar que e possivel recuperar usuários")
+    void FindAll_Should_ReturnAUserList() {
+        List<User> userList = new ArrayList<>();
+
+        userList.add(user1);
+        userList.add(user2);
+
+        when(userRepository.findAllUsers()).thenReturn(userList);
+
+        var users = userService.findAllUsers();
+
+        assertNotNull(users);
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    @DisplayName("Validar que e possivel recuperar um unico usuario pelo id")
+    void FindById_Should_ReturnASingleUser() {
+
+        when(userRepository.findUserById(user1.getId())).thenReturn(user1);
+
+        var foundUser = userService.findUserById(1L);
+
+        assertNotNull(foundUser);
     }
 
 }
